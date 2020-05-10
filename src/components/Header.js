f2d3643f8cbb95a83a4f0fde68796e8bb01ec751
Tomi Akingba <akingba35@gmail.com>
2020-05-10 15:16:34 +0100

    position fixture

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
         borderBottomColor: "grey",
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'black'
+        backgroundColor: 'green'
     },
     text: {
         fontSize: 36,
