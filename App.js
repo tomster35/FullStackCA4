d0c60c6dac09e12084e2161e4178f8a661ed0925
Tomi Akingba <akingba35@gmail.com>
2020-05-11 14:59:48 +0100

    App colour configuration

--- App.js
+++ App.js
@@ -16,11 +16,11 @@ export default function App(props) {
   } else {
     return (
  <View style={styles.container}>
-   
         {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
         <NavigationContainer linking={LinkingConfiguration}>
           <Stack.Navigator>
             <Stack.Screen name="Root" component={BottomTabNavigator} />
+         
           </Stack.Navigator>
         </NavigationContainer>
       </View>
